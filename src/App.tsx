e523f8e892d9120496079af958f5b5df2c00841c
Bartlomiej95 <panfilbartlomiej@gmail.com>
2023-03-29 19:16:12 +0200

    Builded basic structure - install react-router-dom, styled-components and create empty first homepage view

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./theme/GlobalStyle";
+import { theme } from "./theme/mainTheme";
+import { HomePage } from "./views/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage/>}/>
+          </Routes>
+        </Router>
+
+      </ThemeProvider>
+    </>
   );
 }
 
