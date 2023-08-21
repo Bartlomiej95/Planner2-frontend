f65a7df3684945e557cda42f3ed7e6713c91c1e8
Bartlomiej95 <panfilbartlomiej@gmail.com>
2023-08-21 21:05:08 +0200

    Added static data for main page

---
+++ src/data/index.ts
@@ -0,0 +1,65 @@
+export const data = {
+    solutions: [
+        {
+            id: 1,
+            content: 'Organizuj zadania i projekty swojego zespołu',
+        },
+        {
+            id: 2,
+            content: 'Wymieniaj swoje uwagi z innymi członkami zespołu',
+        },
+        {
+            id: 3,
+            content: 'Przydziel odpowiednie osoby do odpowiednich zadań',
+        },
+        {
+            id: 4,
+            content: 'Przeglądaj specyfikacje projektu',
+        }
+    ],
+    functions: [
+        {
+            id: 1,
+            content: 'Zarządzanie zadaniami w firmie lub zespole',
+        },
+        {
+            id: 2,
+            content: 'Tworzenie nowych zadań oraz projektów',
+        },
+        {
+            id: 3,
+            content: 'Śledzenie postępów w projekcie',
+        },
+        {
+            id: 4,
+            content: 'Zarządzanie zadaniami w firmie lub zespole',
+        }
+    ],
+    faq: [
+        {
+            id:1,
+            question: 'Pytanie 1',
+            answer: 'Odpowiedz nr 1 jest bardzo wyczerpująca i nie pozostawiająca żadnych złudzń',
+        },
+        {
+            id:2,
+            question: 'Pytanie 2',
+            answer: 'Lorem ipsum dolor sit ameut feris lorem super alor da elkont',
+        },
+        {
+            id:3,
+            question: 'Pytanie 3',
+            answer: 'Lorem ipsum dolor sit ameut feris lorem super alor da elkont Lorem ipsum dolor sit ameut feris lorem super alor da elkont',
+        },  
+        {
+            id:4,
+            question: 'Pytanie 4',
+            answer: 'Lorem ipsum dolor sit ameut feris lorem super alor da elkont, rem ipsum dolor sit ameut feris lorem super alor da elkont',
+        },
+        {
+            id:5,
+            question: 'Pytanie 5',
+            answer: 'Lorem ipsum dolor sit ameut feris lorem super aloament dolar esu euros lorsie akumel ent ss polksi sois dolor sit emut lorem iosuns',
+        },
+    ]
+}
