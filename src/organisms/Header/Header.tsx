f9493e18bb35ecfb8591026d32b7e75f7b411dd7
Bartlomiej95 <panfilbartlomiej@gmail.com>
2023-08-26 18:45:13 +0200

    Added button "go back" using useNavigate

--- src/organisms/Header/Header.tsx
+++ src/organisms/Header/Header.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import menuIcon from '../../assets/menuIcon.svg';
 import { Logo } from '../../components/Heading/Heading';
 import { HiddenMenu } from '../HiddenMenu/HiddenMenu';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { PrimaryBtn } from '../../components/Button/Button';
 
 const Wrapper = styled.div`
     width: 100vw;
@@ -20,15 +22,57 @@ const Wrapper = styled.div`
     }
 `;
 
+const IconsDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+`;
+
+const Icon = styled.div<{ readonly bcgIcon :string}>`
+    width: 30px;
+    height: 30px;
+    box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.16);
+    border-radius: 50%;
+    margin-left: 30px;
+    cursor: pointer;
+    background-color: white;
+
+    ${({ bcgIcon }) => bcgIcon && css`
+    
+        background-image: url(${bcgIcon});
+        background-position: center;
+        background-repeat: no-repeat;
+    `}
+`;
+
+const BackButton = styled(PrimaryBtn)`
+    width: 60px;
+    height: 30px;
+    font-size: 12px;
+    font-weight: 700;
+    border: none;
+    box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.16);
+    margin-left: 30px;
+`;
+
 const Header = () => {
     const [showHiddenMenu, setShowHiddenMenu] = useState(false);
+
+    const nav = useNavigate();
+    const location = useLocation();
+
+    const searchedLoc = location.pathname === "/" || location.pathname === "/dashbord/user" || location.pathname === "/logout";
+
     return(
         <> 
         {
             !showHiddenMenu && (
                 <Wrapper>
                     <Logo>Planner</Logo>
-                    <img src={menuIcon} alt="ikona menu" onClick={() => setShowHiddenMenu(prev => !prev)}/>
+                    <IconsDiv>
+                        { !searchedLoc && ( <BackButton onClick={() => nav(-1)}>Wróć</BackButton> ) }
+                        <Icon bcgIcon={menuIcon} onClick={() => setShowHiddenMenu(prev => !prev)}/>
+                    </IconsDiv>
                 </Wrapper>
             )
         }
