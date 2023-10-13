a1d31b7c101ce4caed2fe7e8ec08b21a46f236bc
Bartlomiej95 <panfilbartlomiej@gmail.com>
2023-10-13 22:21:24 +0200

    Added MessageModal with communicate to RegisterPage

--- src/views/RegisterPage.tsx
+++ src/views/RegisterPage.tsx
@@ -8,6 +8,8 @@ import { Heading } from "../components/Heading/Heading";
 import { Input } from "../components/Input/Input";
 import { createNewUser } from "../store/Users/usersSlice";
 import { emailValidation } from "../utils/emailValidation";
+import { MessageModal } from "../molecules/Modal/MessageModal";
+import { useAppDispatch } from "../utils/hooks";
 
 const Wrapper = styled.section`
     width: 100%;
@@ -29,9 +31,11 @@ const ErrorMsg = styled.h2`
 
 export const RegisterPage = () => {
 
-    const dispatch = useDispatch<AppDispatch>();
+    const dispatch = useAppDispatch();
     const [ newUserEmail, setNewUserEmail] = useState('');
     const [ validationData, setValidationData ] = useState({ ok: false, message: ""});
+    const [ error, setError ] = useState('');
+    const [ popup, setPopup] = useState('');
 
     const handleChange = (e: React.SyntheticEvent) => {
         e.preventDefault();
@@ -43,9 +47,21 @@ export const RegisterPage = () => {
         e.preventDefault();
         try{
             const validate = emailValidation(newUserEmail);
-            setValidationData(validate); 
             if(validate.ok){
-                dispatch(createNewUser(newUserEmail));
+                (async () => {
+                    const payload = await dispatch(createNewUser(newUserEmail)).then(res => res.payload);
+                    if(payload.ok){
+                        setPopup("Success");
+                    } else {
+                        setPopup("Error");
+                    }
+                    setTimeout(() => setPopup(""), 3000);
+                    setError(payload.message);
+                    })(); 
+            } else {
+                setPopup("Error");
+                setTimeout(() => setPopup(""), 3000);
+                setError(validate.message);
             }
             
         } catch(e){
@@ -54,14 +70,17 @@ export const RegisterPage = () => {
     }
 
     return(
-        <Wrapper>
-            <Header />
-            <RegisterHeading>Formularz rejestracyjny</RegisterHeading>
-            <form>
-                <Input type="email" value={newUserEmail} placeholder="Adres email" onChange={(e) => handleChange(e)} />
-                { !validationData.ok && <ErrorMsg>{validationData.message}</ErrorMsg> }
-                <RegisterBtn onClick={(e) => handleClick(e)}>Zarejestruj się</RegisterBtn>
-            </form>
-        </Wrapper>
+        <>
+            <Wrapper>
+                <Header />
+                <RegisterHeading>Formularz rejestracyjny</RegisterHeading>
+                <form>
+                    <Input type="email" value={newUserEmail} placeholder="Adres email" onChange={(e) => handleChange(e)} />
+                    <RegisterBtn onClick={(e) => handleClick(e)}>Zarejestruj się</RegisterBtn>
+                </form>
+            </Wrapper>
+            { popup === "Error" && <MessageModal type="Error" content={error} /> }  
+            { popup === "Success" && <MessageModal type="Success" content="Na podany mail wysłano link aktywacyjny" /> } 
+        </>
     )
 }
